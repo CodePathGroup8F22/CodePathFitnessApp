c8829a58ab859dbfbb94e3a0cfcb3ad607cb65ac
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2022-11-04 23:03:19 -0700

    Organized HomeViewController

--- FitnessApp/HomeViewController.swift
+++ FitnessApp/HomeViewController.swift
@@ -11,40 +11,22 @@ import Parse
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var workoutGroup = [PFObject]()
-    var imageList = [UIImage(named: "stock1"), UIImage(named: "stock"), UIImage(named: "stock1")]
+    var imageList = [UIImage(named: "stock1"), UIImage(named: "stock"), UIImage(named: "stock2"), UIImage(named:"stock3")]
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workoutGroup.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
-        let group = workoutGroup[indexPath.row]
-        cell.imageLabel.text = group["name"] as! String
-        
-        let imageFile = imageList[indexPath.row % 3] as! UIImage
-        cell.imagePoster.image = imageFile
-        cell.imagePoster.layer.cornerRadius = 30
-        
-        
-//        cell.imagePoster.af.setImage(withURL: url)
-        return cell
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 180
+        getData()
     }
     
     
-    override func viewDidAppear(_ animated: Bool) {
+    @objc func getData() {
         let query = PFQuery(className: "muscleGroup")
         query.includeKey("name")
         query.limit = 20
@@ -63,6 +45,24 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         userLabel.text = user!["firstname"] as! String
         userLabel.isHidden = false
         tempLabel.isHidden = false
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workoutGroup.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+        let group = workoutGroup[indexPath.row]
+        cell.imageLabel.text = group["name"] as! String
+        
+        let imageFile = imageList[indexPath.row % 3] as! UIImage
+        cell.imagePoster.image = imageFile
+        cell.imagePoster.layer.cornerRadius = 30
+        
+        
+//        cell.imagePoster.af.setImage(withURL: url)
+        return cell
     }
     // MARK: - Navigation
 
