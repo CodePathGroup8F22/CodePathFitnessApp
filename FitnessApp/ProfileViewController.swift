3978293764675d1c342a77b7104e274050b05b52
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2022-11-08 00:05:23 -0800

    Small changes

--- FitnessApp/ProfileViewController.swift
+++ FitnessApp/ProfileViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileViewController: UIViewController {
     
     @IBOutlet weak var firstNameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
@@ -17,7 +17,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        getData()
+        getData()
         
         imageView.layer.cornerRadius = (imageView.frame.size.width) / 2
         imageView.clipsToBounds = true
@@ -40,31 +40,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         imageView.af.setImage(withURL: url)
     }
   
-    
-    @IBAction func onProfileButton(_ sender: Any) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            picker.sourceType = .camera
-        }
-        else {
-            picker.sourceType = .photoLibrary
-        }
-        
-        present(picker, animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.editedImage] as! UIImage
-        let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af.imageScaled(to: size)
-        
-        imageView.image = scaledImage
-        dismiss(animated: true, completion: nil)
-    }
-    
     /*
     // MARK: - Navigation
 
